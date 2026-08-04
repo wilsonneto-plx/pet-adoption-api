65d393bcf803be8d676440653085d7858ffccd21
Wilson de Andrade Veloso Neto <wilsonneto@aluno.uespi.br>
2026-08-03 23:59:55 -0300

    test: replace deprecated MockBean with MockitoBean

--- java-exceptions-projeto_inicial/src/test/java/adopet/api/controller/AdocaoControllerTest.java
+++ java-exceptions-projeto_inicial/src/test/java/adopet/api/controller/AdocaoControllerTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -29,7 +28,7 @@ public class AdocaoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AdocaoService service;
 
     @Test
--- java-exceptions-projeto_inicial/src/test/java/adopet/api/controller/TutorControllerTest.java
+++ java-exceptions-projeto_inicial/src/test/java/adopet/api/controller/TutorControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,7 +28,7 @@ public class TutorControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private TutorService service;
 
     @Test
